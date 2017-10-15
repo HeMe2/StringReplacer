6ba8244ff4dec778f1a7c0d0759d8049e0e1ba40
Henning <github@skotax.de>
2017-10-15 18:39:32 +0200

    initial commit of the String replacer functions in the StringReplacer workspace

---
+++ src/StringReplacer.cpp
@@ -0,0 +1,46 @@
+/*
+ * StringReplacer.c
+ *
+ *  Created on: Sep 24, 2017
+ *      Author: HeMe2
+ */
+
+#include "../inc/StringReplacer.h"
+
+string StringReplacer::replaceSubstrings(const string &origin, const string & toReplace, const string & replacement){
+	size_t position = StringReplacer::searchForSubstring(origin, toReplace);
+
+	if (position == origin.size()){
+		return origin;
+	} else {
+		const string &remainingString = origin.substr(position + toReplace.size(), origin.size());
+		return origin.substr(0, position) +
+				replacement +
+				StringReplacer::replaceSubstrings(remainingString, toReplace, replacement) ;
+	}
+}
+
+size_t StringReplacer::searchForSubstring(const string &origin, const string & toSearch) {
+	// boundary checks first
+	size_t bytesToCompare = toSearch.size();
+	if (bytesToCompare < origin.size()) {
+		// search from front to back
+		for (size_t index = 0; index < origin.size(); index++) {
+			if (matches(origin.substr(index, bytesToCompare).c_str(), toSearch.c_str(), bytesToCompare)) {
+				return index;
+			}
+		}
+	}
+	return origin.size();
+}
+
+bool matches(const char * one, const char * two, size_t bytes) {
+	if (bytes > 0) {
+		if (*one == *two) {
+			return matches(one + 1, two + 1, bytes - 1);
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
